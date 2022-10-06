6dea464eb50b8059c963488a4707ebba5d1ca4b5
Dmytro <deekorde@gmail.com>
2022-10-06 23:17:37 +0300

    fixed burger menu

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".navigation");
+let overlay = document.querySelector(".overlay")
 
 mobileMenu.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active-menu")
+    mobileMenu.classList.toggle("active-menu");
     if(mobileMenu.classList.contains("active-menu")){
-        mainMenu.classList.add("active-menu")
+        mainMenu.classList.add("active-menu");
+        document.body.style.overflow = "hidden";
     } else{
-        mainMenu.classList.remove("active-menu")
+        mainMenu.classList.remove("active-menu");
+        document.body.style.overflow = "visible";
     }
+})
+
+overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("active-menu");
+    mainMenu.classList.remove("active-menu");
+    document.body.style.overflow = "visible";
 })
